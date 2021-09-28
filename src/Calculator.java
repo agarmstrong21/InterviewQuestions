ac38f308c88279b8e72fa5bdbd6d49ebacfddfa7
Addison Armstrong <55289031+agarmstrong21@users.noreply.github.com>
2021-09-28 13:34:48 -0500

    Fixed Calculator with Recursion

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,14 +8,22 @@ public class Calculator {
     }
 
     public static int multiply(int a, int b){
+        /*
         int out = 0;
         for(int i = 0; i < a; i++){
             out = addition(out, b);
         }
         return out;
+         */
+
+        if(b == 0){
+            return 0;
+        }
+        return addition(a, multiply(a, b - 1));
     }
 
     public static int division(int a, int b){
+        /*
         int out = 0;
         while(a > 0){
             a = subtraction(a, b);
@@ -25,12 +33,20 @@ public class Calculator {
             }
         }
         return out;
+        */
+
+        if(a == 0){
+            return 0;
+        }else if( a < 0){
+            return -1;
+        }
+        return 1 + division(subtraction(a, b), b);
     }
 
     public static void main(String[] args) {
         System.out.println(addition(10, 10));
         System.out.println(subtraction(10, 10));
         System.out.println(multiply(5, 5));
-        System.out.println(division(10, 3));
+        System.out.println(division(6, 3));
     }
 }
